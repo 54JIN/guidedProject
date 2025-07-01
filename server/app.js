8d810cfa3972ccf2c1f4dc7fe39b43801e6c9edb
Alice Smith <alice@webage.com>
2025-07-01 15:47:19 -0400

    Created Characters Router

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 import express, { json } from 'express';
 import cors from 'cors';
 
+import charactersRouter from './routers/characters.js';
+
 const app = express();
 app.use(cors());
 const PORT = 3001;
 
 app.use(express.json())
+
+app.use(charactersRouter);
 
 app.get('/api/planets', async (req, res) => {
     try {
---
+++ server/routers/characters.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const charactersRouter = new express.Router()
+
+charactersRouter.get('/api/characters', async (req,res) => {
+    
+})
+
+export default charactersRouter;
